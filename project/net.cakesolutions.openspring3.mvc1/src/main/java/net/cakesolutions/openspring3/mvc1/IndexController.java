eccd291e75a9afa0a40dc7e9189537c12ad31947
Jan Machacek <jan.machacek@gmail.com>
2010-06-15 18:34:12 +0100

    More Spring MVC text

--- project/net.cakesolutions.openspring3.mvc1/src/main/java/net/cakesolutions/openspring3/mvc1/IndexController.java
+++ project/net.cakesolutions.openspring3.mvc1/src/main/java/net/cakesolutions/openspring3/mvc1/IndexController.java
@@ -1,5 +1,17 @@
 package net.cakesolutions.openspring3.mvc1;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class IndexController {
+
+	@ResponseBody
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String foo() {
+		return "Hello, world";
+	}
 
 }
